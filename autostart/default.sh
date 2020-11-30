5eb3d183148d78a742ed69c8714d83b838464f8f
eatmyvenom <jacobhagan@outlook.com>
2020-11-29 17:18:00 -0800

    fix

--- autostart/default.sh
+++ autostart/default.sh
@@ -4,7 +4,8 @@ killall -9 sxhkd
 
 picom --experimental-backends -b & 
 xrdb ~/.Xresources
-exec ~/.config/polybar/launch-notawesome.sh &
+#exec ~/.config/polybar/launch-notawesome.sh &
+exec ~/.config/polybar/launch.sh &
 ~/scripts/notifications.sh &
 nitrogen --restore &
 xrandr --output DP-0 --mode 3840x2160 --scale 1x1 --pos 0x0 --output HDMI-0 --mode 1920x1080 --scale 1x1 --pos -1920x0 &
