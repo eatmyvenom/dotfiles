328836edb400e509d10988c2db9fcb7f064da84c
eatmyvenom <jacobhagan@outlook.com>
2021-02-04 14:54:14 -0800

    change some services and background

--- autostart/runit.sh
+++ autostart/runit.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+killall -9 pulseaudio
+killall -9 polybar
+
 cd ~/.dbus
 dbus-launch --sh-syntax --exit-with-session > /tmp/dbus-address-temp-file
 source /tmp/dbus-address-temp-file
@@ -7,5 +10,3 @@ cd ~
 runsvdir ~/dots/runit/runsvdir &
 
 xrdb ~/.Xresources
-xrandr --output DP-0 --mode 3840x2160 --scale 1x1 --pos 0x0 --output HDMI-0 --mode 1920x1080 --scale 1x1 --pos -1920x0 &
-feh --bg-fill ~/Pictures/wallpaper/wallpaper.jpg ~/Pictures/wallpaper/wallpaper2.jpg &  
