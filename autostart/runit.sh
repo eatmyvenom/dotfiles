3b658c09de96be3ec2b3dfbb67477918a1ab1325
eatmyvenom <eat.my.venomm@gmail.com>
2021-02-08 23:51:10 -0800

    update config for fresh arch

--- autostart/runit.sh
+++ autostart/runit.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
-
-cd ~/.dbus
-dbus-launch --sh-syntax --exit-with-session > /tmp/dbus-address-temp-file
-source /tmp/dbus-address-temp-file
-cd ~
 runsvdir ~/dots/runit/runsvdir &
-
 xrdb ~/.Xresources
 xrandr --output DP-0 --mode 3840x2160 --scale 1x1 --pos 0x0 --output HDMI-0 --mode 1920x1080 --scale 1x1 --pos -1920x0 &
 feh --bg-fill ~/Pictures/wallpaper/wallpaper.jpg ~/Pictures/wallpaper/wallpaper2.jpg &  
