5b8d8ed65e88276c66c19dbd03afa4e2973d0386
eatmyvenom <eat.my.venomm@gmail.com>
2021-02-21 13:16:34 -0800

    change autostart for xwinwrap

--- autostart/runit.sh
+++ autostart/runit.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
+xrandr --output DP-0 --mode 3840x2160 --scale 1x1 --pos 0x0 --output HDMI-0 --mode 1920x1080 --scale 1x1 --pos -1920x0
 runsvdir ~/dots/runit/runsvdir &
 xrdb ~/.Xresources
-xrandr --output DP-0 --mode 3840x2160 --scale 1x1 --pos 0x0 --output HDMI-0 --mode 1920x1080 --scale 1x1 --pos -1920x0 &
-feh --bg-fill ~/Pictures/wallpaper/wallpaper.jpg ~/Pictures/wallpaper/wallpaper2.jpg &  
+hsetroot -solid '#181c30'
