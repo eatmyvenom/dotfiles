1494044a371ab507cf05c4cbb8b4663320e2828c
eatmyvenom <eat.my.venomm@gmail.com>
2023-01-14 20:12:07 -0800

    update all

--- autostart/runit.sh
+++ autostart/runit.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-xrandr --output DP-0 --mode 3840x2160 --scale 1x1 --pos 0x0 --output HDMI-0 --mode 1920x1080 --scale 1x1 --pos -1920x0
+xrandr --output DP-0 --primary --mode 3840x2160 --rate 144.00 --pos 0x0 --output USB-C-0.2 --mode 3840x2160 --left-of DP-0
 runsvdir ~/dots/runit/runsvdir &
 xrdb ~/.Xresources
+ssh-agent
 hsetroot -solid '#181c30'
--- shells/.zsh.d/activate.sh
+++
@@ -1,1 +0,0 @@
-/usr/share/autoenv-git/activate.sh
